fb5d7dbf94c5d2cce490fd71eb0e9aa5300d5935
sowfkun <17521184@gm.uit.edu.vn>
2021-08-23 16:13:30 +0700

    abc

--- 2. first-react-app/src/components/traffic-light/TrafficLight.js
+++ 2. first-react-app/src/components/traffic-light/TrafficLight.js
@@ -7,8 +7,34 @@ const YELLOW = 1;
 const GREEN = 2;
 
 class TrafficLight extends Component {
+  constructor() {
+    super();
+    this.state = {
+      currentColor: RED,
+    };
+
+    setInterval(() => {
+      this.setState({
+        currentColor: this.getNextColor(this.state.currentColor),
+      });
+    }, 1000);
+  }
+
+  getNextColor(color) {
+    switch (color) {
+      case GREEN:
+        return YELLOW;
+      case YELLOW:
+        return RED;
+      case RED:
+        return GREEN;
+      default:
+        return RED;
+    }
+  }
+
   render() {
-    const { currentColor } = this.props;
+    const { currentColor } = this.state;
     return (
       <div className='TrafficLight'>
         {/** Dynamic classname use classnames module */}
---
+++ 2. first-react-app/src/stories/Accordion.stories.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Accordion from '../components/accordion/Accordion';
+
+export default {
+  title: 'Accordion',
+  component: Accordion,
+};
+
+const Template = (args) => <Accordion {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  heading: 'Heading',
+  content: 'Content',
+  children: 'Child Content',
+  isCollapsed: true
+};
